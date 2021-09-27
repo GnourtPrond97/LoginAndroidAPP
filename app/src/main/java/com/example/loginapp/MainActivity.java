7e91020cc9d8998714914dfec507adb4d79b58c4
GnourtPrond97 <truonghhth1905001@gmail.com>
2021-09-27 21:20:13 +0700

    cm

--- app/src/main/java/com/example/loginapp/MainActivity.java
+++ app/src/main/java/com/example/loginapp/MainActivity.java
@@ -1,63 +1,50 @@
 package com.example.loginapp;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
-    private ListView lvContact;
-    private List<ContactModel> listContacts = new ArrayList<>();
+
 
     @Override
 
     protected void  onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initData();
-        lvContact =(ListView) findViewById(R.id.lvContact);
-        ContactAdapter  adapter = new ContactAdapter(listContacts,this);
-        lvContact.setAdapter(adapter);
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w("1", "Fetching FCM registration token failed", task.getException());
+                            return;
+                        }
 
-        lvContact.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id    ) {
-                ContactModel model = listContacts.get(position);
-                Toast.makeText(MainActivity.this,model.getName(),Toast.LENGTH_SHORT).show();
-            }
-        });
+                        // Get new FCM registration token
+                        String token = task.getResult();
+
+                        // Log and toast
+//                        String msg = getString(R.string.app_name, token);
+                        Log.d("1", token    );
+                        Toast.makeText(MainActivity.this, token, Toast.LENGTH_SHORT).show();
+                    }
+                });
+
     }
 
-    private void initData() {
-        ContactModel contact = new ContactModel(" nguyen van A","919149198",R.drawable.ic_avatar1);
-        listContacts.add(contact);
-        contact = new ContactModel(" nguyen van B","124122141",R.drawable.ic_avatar2);
-        listContacts.add(contact);
-        contact = new ContactModel(" nguyen van c","124124124",R.drawable.ic_avatar3);
-        listContacts.add(contact);
-
-        contact = new ContactModel(" nguyen van d","134613136",R.drawable.ic_avatar4);
-        listContacts.add(contact);
-        contact = new ContactModel(" nguyen van B","385664644",R.drawable.ic_avatar2);
-        listContacts.add(contact);
-        contact = new ContactModel(" nguyen van c","532624774",R.drawable.ic_avatar3);
-        listContacts.add(contact);
-
-        contact = new ContactModel(" nguyen van d","548465353",R.drawable.ic_avatar4);
-        listContacts.add(contact);
-        contact = new ContactModel(" nguyen van B","525869432",R.drawable.ic_avatar2);
-        listContacts.add(contact);
-        contact = new ContactModel(" nguyen van c","346375611",R.drawable.ic_avatar3);
-        listContacts.add(contact);
-
-        contact = new ContactModel(" nguyen van d","322346464",R.drawable.ic_avatar4);
-        listContacts.add(contact);
-    }
 }
